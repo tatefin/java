db06b8ea81241a946d34d8cfb3ff4f73f5a1b444
Samu "tate" Lehtonen <samusorsa@gmail.com>
2025-03-06 10:24:30 +0200

    Update Main.java
    Lisätty + merkki jotta koodi toimii ilman erroreita.

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
     // Käytetään EndianChecker-luokkaa tarkistamaan endianness
         String endianType = EndianChecker.checkEndian();
-        System.out.println("Järjestelmän endianness on: " endianType);
+        System.out.println("Järjestelmän endianness on: " + endianType);
     }
 }
 
